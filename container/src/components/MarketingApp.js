03711968b8428e9108f6026f4abc80932e2bdd97
Kishan Kumar Saini <kishan.saini@tavant.com>
2024-09-14 00:06:47 +0530

    initial commit

---
+++ container/src/components/MarketingApp.js
@@ -0,0 +1,13 @@
+import { mount } from "marketingApp/Marketing";
+
+import React, { useRef, useEffect } from "react";
+
+export default () => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    mount(ref.current);
+  }, []);
+
+  return <div ref={ref} />;
+};
